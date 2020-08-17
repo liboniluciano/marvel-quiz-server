23451286b74ad8b5ba4969177c7ef6501532bfe8
Luciano Liboni <luciano_liboni@hotmail.com>
2020-08-17 16:33:54 -0300

    fix: package

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,10 @@
+import path from 'path';
 
 module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL,
   migrations: {
-    directory: __dirname + 'src/database/migrations'
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
   },
   useNullAsDefault: true,
 };
