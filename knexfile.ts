48746d928a96721ffc18dc938696f026bc9c2c16
Luciano Liboni <luciano_liboni@hotmail.com>
2020-08-17 15:30:46 -0300

    fix: production setup

--- knexfile.ts
+++ knexfile.ts
@@ -2,13 +2,7 @@ import path from 'path';
 
 module.exports = {
   client: 'pg',
-  connection: {
-    host: process.env.DB_HOST_PROD,
-    port: 5434,
-    user: process.env.DB_HOST_PROD,
-    password: process.env.DB_HOST_PROD,
-    database: process.env.DB_HOST_PROD
-  },
+  connection: process.env.DATABASE_URL,
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
   },
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,14 +1,8 @@
 import knex from 'knex';
 
 const db = knex({
-  client: 'postgres',
-  connection: {
-    host: process.env.DB_HOST_PROD,
-    port: 5434,
-    user: process.env.DB_HOST_PROD,
-    password: process.env.DB_HOST_PROD,
-    database: process.env.DB_HOST_PROD
-  },
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
   useNullAsDefault: true
 })
 
